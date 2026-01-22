21772b5893e58bafcebfe609f5706caee05941f4
29Gabriel29 <88162157+29Gabriel29@users.noreply.github.com>
2026-01-22 18:55:25 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,13 @@ define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASS', getenv('DB_PASS'));
 
+$conn = new PDO(
+    "pgsql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+    DB_USER,
+    DB_PASS,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
+
 // Resend
 define('RESEND_API_KEY', getenv('RESEND_API_KEY'));
 define('RESEND_FROM', getenv('RESEND_FROM'));
