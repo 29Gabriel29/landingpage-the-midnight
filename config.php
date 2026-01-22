e60398367304de67e9e0f8bfd156e04995516108
29Gabriel29 <88162157+29Gabriel29@users.noreply.github.com>
2026-01-22 18:44:58 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Database
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+
+// Resend
+define('RESEND_API_KEY', getenv('RESEND_API_KEY'));
+define('RESEND_FROM', getenv('RESEND_FROM'));
